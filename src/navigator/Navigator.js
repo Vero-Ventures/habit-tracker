9194318491f75694f4f7376a81f382db446c9b96
Matthew Siggs <mcsiggs@shaw.ca>
2024-05-07 11:27:28 -0700

    fixing eslint precommit errors #26

--- src/navigator/Navigator.js
+++ src/navigator/Navigator.js
@@ -1,28 +1,90 @@
-import React from "react";
-import { Image } from "react-native";
-import Colors from "../../assets/styles/Colors";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import React from 'react';
+import { Image } from 'react-native';
+import Colors from '../../assets/styles/Colors';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 const Tab = createBottomTabNavigator();
+
+// testing eslint with a comment
 
 export default function Navigator() {
   const icons = (route, focused) => {
     const sizeStyle = { width: 24, height: 24 };
 
     switch (route.name) {
-      case "Home":
-        return <Image source={focused ? require("../../assets/icons/home-selected.png") : require("../../assets/icons/home.png")} style={sizeStyle} />;
-      case "ExtraTips":
-        return <Image source={focused ? require("../../assets/icons/info-selected.png") : require("../../assets/icons/info.png")} style={sizeStyle} />;
-      case "Stakes":
-        return <Image source={focused ? require("../../assets/icons/stakes-selected.png") : require("../../assets/icons/stakes.png")} style={sizeStyle} />;
-      case "My Habits":
-        return <Image source={focused ? require("../../assets/icons/activity-selected.png") : require("../../assets/icons/activity.png")} style={focused ? { width: 22, height: 20 } : sizeStyle} />;
-      case "Community":
-        return <Image source={focused ? require("../../assets/icons/users-selected.png") : require("../../assets/icons/users.png")} style={sizeStyle} />;
-      case "Profile":
-        return <Image source={focused ? require("../../assets/icons/user-selected.png") : require("../../assets/icons/user.png")} style={sizeStyle} />;
+      case 'Home':
+        // return <Image source={focused ? require("../../assets/icons/home-selected.png") : require("../../assets/icons/home.png")} style={sizeStyle} />;
+        return (
+          <Image
+            source={
+              focused
+                ? '../../assets/icons/home-selected.png'
+                : '../../assets/icons/home.png'
+            }
+            style={sizeStyle}
+          />
+        );
+      case 'ExtraTips':
+        // return <Image source={focused ? require("../../assets/icons/info-selected.png") : require("../../assets/icons/info.png")} style={sizeStyle} />;
+        return (
+          <Image
+            source={
+              focused
+                ? '../../assets/icons/info-selected.png'
+                : '../../assets/icons/info.png'
+            }
+            style={sizeStyle}
+          />
+        );
+      case 'Stakes':
+        // return <Image source={focused ? require("../../assets/icons/stakes-selected.png") : require("../../assets/icons/stakes.png")} style={sizeStyle} />;
+        return (
+          <Image
+            source={
+              focused
+                ? '../../assets/icons/stakes-selected.png'
+                : '../../assets/icons/stakes.png'
+            }
+            style={sizeStyle}
+          />
+        );
+      case 'My Habits':
+        // return <Image source={focused ? require("../../assets/icons/activity-selected.png") : require("../../assets/icons/activity.png")} style={focused ? { width: 22, height: 20 } : sizeStyle} />;
+        return (
+          <Image
+            source={
+              focused
+                ? '../../assets/icons/activity-selected.png'
+                : '../../assets/icons/activity.png'
+            }
+            style={focused ? { width: 22, height: 20 } : sizeStyle}
+          />
+        );
+      case 'Community':
+        // return <Image source={focused ? require("../../assets/icons/users-selected.png") : require("../../assets/icons/users.png")} style={sizeStyle} />;
+        return (
+          <Image
+            source={
+              focused
+                ? '../../assets/icons/users-selected.png'
+                : '../../assets/icons/users.png'
+            }
+            style={sizeStyle}
+          />
+        );
+      case 'Profile':
+        // return <Image source={focused ? require("../../assets/icons/user-selected.png") : require("../../assets/icons/user.png")} style={sizeStyle} />;
+        return (
+          <Image
+            source={
+              focused
+                ? '../../assets/icons/user-selected.png'
+                : '../../assets/icons/user.png'
+            }
+            style={sizeStyle}
+          />
+        );
       default:
         return null;
     }
@@ -44,8 +106,7 @@ export default function Navigator() {
             paddingTop: 20,
             paddingBottom: 24,
           },
-        })}
-      >
+        })}>
         <Tab.Screen name="Home" component={() => <></>} />
         <Tab.Screen name="Stakes" component={() => <></>} />
         <Tab.Screen name="My Habits" component={() => <></>} />
