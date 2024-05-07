f7723d70bbf7e30bc6675159e315b403f0960889
Laurie Solkoski <lsolkoski3@my.bcit.ca>
2024-05-06 18:57:00 -0700

    added skeleton navigator, style files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,36 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import pluginReact from 'eslint-plugin-react';
+import recommendedReactConfig from 'eslint-plugin-react/configs/recommended.js';
 
 export default [
-  { languageOptions: { globals: globals.browser } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        require: "readonly" // manually define 'require' if used 
+      },
+    },
+    parserOptions: {
+      ecmaVersion: 2020, // ECMAScript version
+      sourceType: 'module', // set module type
+      ecmaFeatures: {
+        jsx: true,  // enable JSX 
+      }
+    },
+    settings: {
+      react: {
+        version: "detect", // automatically detect React version
+      },
+    },
+  },
   pluginJs.configs.recommended,
-  pluginReactConfig,
+  {
+    ...recommendedReactConfig,
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
 ];
