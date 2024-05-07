d66e7047aa6d366a5725b4d88e6504cf2cb39fc9
Laurie Solkoski <lsolkoski3@my.bcit.ca>
2024-05-06 18:58:00 -0700

    i tried to configure this but reverted it back to original

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,9 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import pluginReact from 'eslint-plugin-react';
-import recommendedReactConfig from 'eslint-plugin-react/configs/recommended.js';
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js';
 
 export default [
-  {
-    languageOptions: {
-      globals: {
-        ...globals.browser,
-        require: "readonly" // manually define 'require' if used 
-      },
-    },
-    parserOptions: {
-      ecmaVersion: 2020, // ECMAScript version
-      sourceType: 'module', // set module type
-      ecmaFeatures: {
-        jsx: true,  // enable JSX 
-      }
-    },
-    settings: {
-      react: {
-        version: "detect", // automatically detect React version
-      },
-    },
-  },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
-  {
-    ...recommendedReactConfig,
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-  },
+  pluginReactConfig,
 ];
